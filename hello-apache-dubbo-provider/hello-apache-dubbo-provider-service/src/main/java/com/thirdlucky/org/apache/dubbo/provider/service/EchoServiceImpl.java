008e16f2c7e3958a7f34971366212a201429e26a
thirdlucky <thirdlucky@126.com>
2020-02-11 17:18:22 +0800

    dubbo kryo and loadbalance

--- hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/thirdlucky/org/apache/dubbo/provider/service/EchoServiceImpl.java
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/thirdlucky/org/apache/dubbo/provider/service/EchoServiceImpl.java
@@ -3,12 +3,15 @@ package com.thirdlucky.org.apache.dubbo.provider.service;
 
 import com.thirdlucky.org.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service(version = "1.0.0")
 public class EchoServiceImpl implements EchoService {
 
+    @Value("${dubbo.protocol.port}")
+    private String port;
     @Override
     public String echo(String echo) {
-        return "Echo hello " + echo;
+        return "Hello dubbo my port is:" + port + " " + echo;
     }
 }
