c26c64151ed7bb35b2c585da6d028aa0b558fc1c
thirdlucky <thirdlucky@126.com>
2020-02-11 17:42:53 +0800

    dubbo config from nacos

--- hello-apache-dubbo-consumer/src/main/java/com/thirdlucky/org/apache/dubbo/consumer/controller/EchoController.java
+++ hello-apache-dubbo-consumer/src/main/java/com/thirdlucky/org/apache/dubbo/consumer/controller/EchoController.java
@@ -2,19 +2,25 @@ package com.thirdlucky.org.apache.dubbo.consumer.controller;
 
 import com.thirdlucky.org.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class EchoController {
 
-	@Reference(version = "1.0.0")
-	private EchoService service;
+    @Value("${user.name}")
+    private String username;
 
-	@GetMapping("echo/{text}")
-	public String echo(@PathVariable("text") String text) {
-		return service.echo(text);
-	}
+    @Reference(version = "1.0.0")
+    private EchoService service;
+
+    @GetMapping("echo/{text}")
+    public String echo(@PathVariable("text") String text) {
+        return service.echo(text) + username;
+    }
 
 }
